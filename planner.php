335ab1f731d4bae7a364b51801416e6928e6417d
YoungSeok Joo <robinjoo1015@naver.com>
2019-07-31 06:33:02 +0900

    Edit calander php

--- planner.php
+++ planner.php
@@ -15,6 +15,12 @@
     <link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.css" />
     <link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.css" />
 
+    <link href='https://unpkg.com/@fullcalendar/core/main.min.css' rel='stylesheet' />
+    <link href='https://unpkg.com/@fullcalendar/daygrid/main.min.css' rel='stylesheet' />
+    <link href='https://unpkg.com/@fullcalendar/timegrid/main.min.css' rel='stylesheet' />
+
+
+
     <style>
       html, th {
         text-align: center;
@@ -58,8 +64,29 @@
           <button id="test-btn" type="button">Click</button>
         </div>
         <div class="col-md-8">
-          <div id="calendar" style="height: 80vh"></div>
+          <div id='calendar' style='margin:10px; width:800px; height:80vh; float:right;'></div>
         </div>
+      </div>
+    </div>
+
+    <button id='modal_click' style='display: none;' data-toggle="modal" data-target="#myModal">Add Event</button>
+
+    <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+
+        <div class="modal-content">
+          <!--<div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Modal Header</h4>
+          </div>-->
+          <div class="modal-body" style="height: 80vh; overflow: auto">
+            <div id='calendar2'></div>
+          </div>
+          <div class="modal-footer">
+            <button id="add-event" type="button" class="btn btn-default" data-dismiss="modal">Add Event</button>
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -68,6 +95,11 @@
 
     <script src="https://uicdn.toast.com/tui.code-snippet/latest/tui-code-snippet.js"></script>
     <script src="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.js"></script>
+
+    <script src='https://unpkg.com/@fullcalendar/core/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/daygrid/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/timegrid/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/interaction/main.min.js'></script>
 
     <script>
       function getRandomColor() {
@@ -81,7 +113,7 @@
 
       $(document).ready(function() {
         var nMembers = 0;
-
+/*
         var Calendar = tui.Calendar;
         var calendar = new Calendar('#calendar', {
           defaultView: 'month',
@@ -94,7 +126,7 @@
             }
           }
         });
-
+*/
         $("#btn-member-add").click(function() {
           var id = "member" + nMembers;
           var name = $("#input-member-name").val();
@@ -128,6 +160,61 @@
           calendar.next();
         });
       });
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          plugins: [ 'interaction', 'dayGrid' ],
+          defaultView: 'dayGridMonth',
+          header: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+          },
+          selectable: true,
+
+
+          events: [
+            {
+                  
+            },
+
+          ],
+
+          dateClick: function(info) {
+
+            document.getElementById("modal_click").click();
+
+            var calendarEl2 = document.getElementById('calendar2');
+
+            var calendar2 = new FullCalendar.Calendar(calendarEl2, {
+              plugins: [ 'interaction', 'timeGrid' ],
+              defaultView: 'timeGridDay',
+              defaultDate: info.dateStr,
+              header: {
+                left: 'none',
+                center: 'title',
+                right: 'none'
+              },
+              height: 'auto',
+              selectable: true,
+            });
+
+            calendar2.render();
+            calendar2.updateSize();
+            calendar2.updateSize();
+
+            $("#myModal").on("hidden.bs.modal", function() {
+              calendar2.destroy();
+            });
+          }
+
+        });
+
+        calendar.render();
+      });
+
     </script>
   </body>
 </html>
