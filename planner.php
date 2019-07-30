b58aa8d6216f33781df12e61ac48780d90dfbba5
Unchun Yang(양운천) <ucyang@ucyang.com>
2019-07-31 07:37:16 +0900

    Refactoring

--- planner.php
+++ planner.php
@@ -95,12 +95,13 @@ if (!isset($_GET['id'])) {
 
     <div class="modal fade" id="myModal" role="dialog">
       <div class="modal-dialog">
-
         <div class="modal-content">
-          <!--<div class="modal-header">
+          <!--
+          <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
             <h4 class="modal-title">Modal Header</h4>
-          </div>-->
+          </div>
+          -->
           <div class="modal-body" style="height: 80vh; overflow: auto">
             <div id='calendar2'></div>
           </div>
